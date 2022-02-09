848b87f3212d4a597f35159a2706bee0456b8d17
Sanpi <sanpi@homecomputing.fr>
2022-02-09 18:08:41 +0100

    Improves render code lisibility

--- src/render.rs
+++ src/render.rs
@@ -1,5 +1,3 @@
-use std::io::Write;
-
 #[derive(Clone, PartialEq, Eq, Debug)]
 pub enum Option {
     NoEdgeLabels,
@@ -43,76 +41,11 @@ where
         + crate::GraphWalk<'a, Node = N, Edge = E, Subgraph = S>,
     W: std::io::Write,
 {
-
     writeln!(w, "{} {} {{", g.kind().keyword(), g.graph_id()?.as_slice())?;
 
     render_subgraphs(g, &g.subgraphs(), w, options)?;
     render_nodes(g, &g.nodes(), w, options)?;
-
-    let mut text = Vec::new();
-    for e in g.edges().iter() {
-        let escaped_label = &g.edge_label(e).to_dot_string();
-        let start_arrow = g.edge_start_arrow(e);
-        let end_arrow = g.edge_end_arrow(e);
-        let start_arrow_s = start_arrow.to_dot_string();
-        let end_arrow_s = end_arrow.to_dot_string();
-
-        write!(w, "    ")?;
-        let source = g.source(e);
-        let target = g.target(e);
-        let source_id = g.node_id(&source)?;
-        let target_id = g.node_id(&target)?;
-
-        write!(
-            text,
-            "{} {} {}",
-            source_id.as_slice(),
-            g.kind().edgeop(),
-            target_id.as_slice()
-        )?;
-
-        if !options.contains(&self::Option::NoEdgeLabels) {
-            write!(text, "[label={}]", escaped_label)?;
-        }
-
-        let style = g.edge_style(e);
-        if !options.contains(&self::Option::NoEdgeStyles) && style != crate::Style::None {
-            write!(text, "[style=\"{}\"]", style.as_slice())?;
-        }
-
-        let color = g.edge_color(e);
-        if !options.contains(&self::Option::NoEdgeColors) {
-            if let Some(c) = color {
-                let colorstring = c.to_dot_string();
-                write!(text, "[color=")?;
-                write!(text, "{}", colorstring)?;
-                write!(text, "]")?;
-            }
-        }
-
-        if !options.contains(&self::Option::NoArrows)
-            && (!start_arrow.is_default() || !end_arrow.is_default())
-        {
-            write!(text, "[")?;
-            if !end_arrow.is_default() {
-                write!(text, "arrowhead=\"")?;
-                write!(text, "{}", end_arrow_s)?;
-                write!(text, "\"")?;
-            }
-            if !start_arrow.is_default() {
-                write!(text, " dir=\"both\" arrowtail=\"")?;
-                write!(text, "{}", start_arrow_s)?;
-                write!(text, "\"")?;
-            }
-
-            write!(text, "]")?;
-        }
-
-        writeln!(text, ";")?;
-        w.write_all(&text)?;
-
-        text.clear();
-    }
+    render_edges(g, &g.edges(), w, options)?;
 
     writeln!(w, "}}")?;
 
@@ -134,39 +67,32 @@ fn render_subgraphs<
     options: &[crate::render::Option],
 ) -> crate::Result {
     for s in subgraphs.iter() {
-        let label;
-        let shape;
+        write!(w, "subgraph ")?;
 
-        let mut text = "subgraph ".to_string();
         let id = g
             .subgraph_id(s)
             .map(|x| format!("{} ", x.name))
             .unwrap_or_default();
 
-        text.push_str(&id);
+        write!(w, "{}", &id)?;
 
-        text.push_str("{\n");
+        writeln!(w, "{{")?;
 
         if !options.contains(&crate::render::Option::NoNodeLabels) {
-            label = format!("label={};\n", g.subgraph_label(s).to_dot_string());
-            text.push_str(&label);
+            let label = format!("label={};\n", g.subgraph_label(s).to_dot_string());
+            write!(w, "{label}")?;
         }
 
         let style = g.subgraph_style(s);
         if !options.contains(&crate::render::Option::NoNodeStyles) && style != crate::Style::None {
-            text.push_str("style=\"");
-            text.push_str(style.as_slice());
-            text.push_str("\";\n");
+            writeln!(w, "style=\"{}\";", style.as_slice())?;
         }
 
         if let Some(s) = g.subgraph_shape(s) {
-            shape = s.to_dot_string();
-            text.push_str("shape=\"");
-            text.push_str(&shape);
-            text.push_str(";\n");
+            write!(w, "shape=\"{}\";", s.to_dot_string())?;
         }
 
-        writeln!(w, "{}", text)?;
+        writeln!(w)?;
 
         for n in g.subgraph_nodes(s).iter() {
             writeln!(w, "{};", g.node_id(n)?.as_slice())?;
@@ -204,7 +130,7 @@ where
             None
         }
     }) {
-        font = format!(r#"fontname="{}""#, fontname);
+        font = format!(r#"fontname="{fontname}""#);
         graph_attrs.push(&font[..]);
         content_attrs.push(&font[..]);
     }
@@ -223,43 +149,110 @@ where
         writeln!(w, r#"    edge[{}];"#, content_attrs_str)?;
     }
 
-    let mut text = Vec::new();
-
     for n in nodes.iter() {
         write!(w, "    ")?;
         let id = g.node_id(n)?;
 
         let escaped = &g.node_label(n)?.to_dot_string();
 
-        write!(text, "{}", id.as_slice())?;
+        write!(w, "{}", id.as_slice())?;
 
         if !options.contains(&self::Option::NoNodeLabels) {
-            write!(text, "[label={}]", escaped)?;
+            write!(w, "[label={escaped}]")?;
         }
 
         let style = g.node_style(n);
         if !options.contains(&self::Option::NoNodeStyles) && style != crate::Style::None {
-            write!(text, "[style=\"{}\"]", style.as_slice())?;
+            write!(w, "[style=\"{}\"]", style.as_slice())?;
         }
 
         let color = g.node_color(n);
         if !options.contains(&self::Option::NoNodeColors) {
             if let Some(c) = color {
                 let colorstring = c.to_dot_string();
-                write!(text, "[color=")?;
-                write!(text, "{}", colorstring)?;
-                write!(text, "]")?;
+                write!(w, "[color={colorstring}]")?;
             }
         }
 
         if let Some(s) = g.node_shape(n) {
-            write!(text, "[shape={}]", &s.to_dot_string())?;
+            write!(w, "[shape={}]", &s.to_dot_string())?;
         }
 
-        writeln!(text, ";")?;
-        w.write_all(&text)?;
+        writeln!(w, ";")?;
+    }
 
-        text.clear();
+    Ok(())
+}
+
+pub fn render_edges<'a, N, E, S, G, W>(
+    g: &'a G,
+    edges: &crate::Edges<'a, E>,
+    w: &mut W,
+    options: &[crate::render::Option],
+) -> crate::Result
+where
+    N: Clone + 'a,
+    E: Clone + 'a,
+    S: Clone + 'a,
+    G: crate::Labeller<'a, Node = N, Edge = E, Subgraph = S>
+        + crate::GraphWalk<'a, Node = N, Edge = E, Subgraph = S>,
+    W: std::io::Write,
+{
+    for e in edges.iter() {
+        let escaped_label = &g.edge_label(e).to_dot_string();
+        let start_arrow = g.edge_start_arrow(e);
+        let end_arrow = g.edge_end_arrow(e);
+        let start_arrow_s = start_arrow.to_dot_string();
+        let end_arrow_s = end_arrow.to_dot_string();
+
+        write!(w, "    ")?;
+        let source = g.source(e);
+        let target = g.target(e);
+        let source_id = g.node_id(&source)?;
+        let target_id = g.node_id(&target)?;
+
+        write!(
+            w,
+            "{} {} {}",
+            source_id.as_slice(),
+            g.kind().edgeop(),
+            target_id.as_slice()
+        )?;
+
+        if !options.contains(&self::Option::NoEdgeLabels) {
+            write!(w, "[label={escaped_label}]")?;
+        }
+
+        let style = g.edge_style(e);
+        if !options.contains(&self::Option::NoEdgeStyles) && style != crate::Style::None {
+            write!(w, "[style=\"{}\"]", style.as_slice())?;
+        }
+
+        let color = g.edge_color(e);
+        if !options.contains(&self::Option::NoEdgeColors) {
+            if let Some(c) = color {
+                let colorstring = c.to_dot_string();
+                write!(w, "[color=")?;
+                write!(w, "{}", colorstring)?;
+                write!(w, "]")?;
+            }
+        }
+
+        if !options.contains(&self::Option::NoArrows)
+            && (!start_arrow.is_default() || !end_arrow.is_default())
+        {
+            write!(w, "[")?;
+            if !end_arrow.is_default() {
+                write!(w, "arrowhead=\"{end_arrow_s}\"")?;
+            }
+            if !start_arrow.is_default() {
+                write!(w, " dir=\"both\" arrowtail=\"{start_arrow_s}\"")?;
+            }
+
+            write!(w, "]")?;
+        }
+
+        writeln!(w, ";")?;
     }
 
     Ok(())
